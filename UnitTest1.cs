9fc1ea50fa90682716aad6f2fea6f144da097d33
MarckRDA <marcos.edo132@gmail.com>
2020-09-27 19:23:39 -0300

    Added some comments

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,11 +4,26 @@ using Xunit;
 namespace Test_tupla_theory
 {
     public class UnitTest1
-    {
+    { 
+        /*Para aceitar coleções do tipo tupla, há a necessidade de transformá-las em tipos IEnumerable
+         Porém, o tipo TheoryData já nos facilita há implementar essa técnica.
+         Abaixo está implementada um método onde irá retornar tuplas do tipo TheoryData, onde o meu decorator [Theory]
+         irá aceitar esse tipo de dado e assim se comportar como um "InlineData"
+         
+        */
+        // Aqui apenas é definido que meu método genérico será do tipo (int, int, int) <= uma tupla, certo?
         public static TheoryData<(int n1, int n2, int expectedResult)> MeuTestTupla()
         {
+            /*
+                Aqui eu apenas irei retornar a quantidade de tuplas que serão meus cenários no teste mais Abaixo
+                Perceba que a definição do "return new T.....<(int, int, int)>" é a mais definição da assinatura do meu método.
+            */
             return new TheoryData<(int n1, int n2, int expectedResult)>
             {
+                /*
+                Defino aqui meus possíveis cenários para o teste. Note que é a mesma definição do inlineData, porém aqui será 
+                o retorno do meu metodo. Lembre-se => [InlineData( 1, 1, 2)] seria a mesma coisa se pudesse passar a tupla como parametro desse tipo.
+                */
                 (1,1,2),
                 (3,4,7),
                 (5,5,10),
@@ -17,9 +32,12 @@ namespace Test_tupla_theory
         }
 
         [Theory]
+        // Usamos o MemberData pois ele nos permite enviar objetos que necessitam de instancias onde o InlineData não nos permitiria
+        // O nameof é onde passaremos o nome do método que criamos para serem executadas nossos cenários definidos no método acima.
         [MemberData(nameof(MeuTestTupla))]
         public void Should_Return_A_Sum_Between_Two_Numbers((int n1, int n2, int expectedResult) numbers)
         {
+            // Daqui em diante é o mesmo de sempre.
             var instancia = new TuplaTest();
 
             var result = instancia.soma((numbers.n1, numbers.n2));
