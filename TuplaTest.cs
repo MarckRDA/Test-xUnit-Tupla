b3438ab9b53a23921af38d77caf9ce892726b61c
MarckRDA <marcos.edo132@gmail.com>
2020-09-28 16:51:50 -0300

    Added list method

--- TuplaTest.cs
+++ TuplaTest.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Test_tupla_theory
 {
     public class TuplaTest
@@ -5,6 +8,18 @@ namespace Test_tupla_theory
         public int soma((int n1, int n2) numeros)
         {
             return numeros.n1 + numeros.n2;
+        }
+
+        public int soma(List<int> numbers)
+        {
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;                
+            }
+
+            return sum;
         }
     }
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Test_tupla_theory
 {
     public class UnitTest1
-    { 
+    {
         /*Para aceitar coleções do tipo tupla, há a necessidade de transformá-las em tipos IEnumerable
          Porém, o tipo TheoryData já nos facilita há implementar essa técnica.
          Abaixo está implementada um método onde irá retornar tuplas do tipo TheoryData, onde o meu decorator [Theory]
@@ -31,6 +32,14 @@ namespace Test_tupla_theory
             };
         }
 
+        public static TheoryData<(List<int>, int expectedResult)> MeuTestLista()
+        {
+            return new TheoryData<(List<int>, int expectedResult)>
+            {
+                (new List<int> {1, 2, 3, 4, 5, 6, 7}, 28),
+                (new List<int> {10, 20, 30, 11}, 71)
+            };
+        }
         [Theory]
         // Usamos o MemberData pois ele nos permite enviar objetos que necessitam de instancias onde o InlineData não nos permitiria
         // O nameof é onde passaremos o nome do método que criamos para serem executadas nossos cenários definidos no método acima.
@@ -41,6 +50,19 @@ namespace Test_tupla_theory
             var instancia = new TuplaTest();
 
             var result = instancia.soma((numbers.n1, numbers.n2));
+
+            Assert.Equal(numbers.expectedResult, result);
+        }
+
+        
+        [Theory]
+        [MemberData(nameof(MeuTestLista))]
+        public void Should_Return_A_Sum((List<int>, int expectedResult) numbers)
+        {
+            // Daqui em diante é o mesmo de sempre.
+            var instancia = new TuplaTest();
+
+            var result = instancia.soma(numbers.Item1);
 
             Assert.Equal(numbers.expectedResult, result);
         }
